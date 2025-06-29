6957cb85b7fcda16f6e921529ff1b8e53466f778
Shipra Singh <168621165+Shiprasingh02@users.noreply.github.com>
2025-06-29 21:01:48 +0530

    Moq hands on week 2
    Week 2 Hands On

---
+++ MailSender.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerCommLib
+{
+    public interface IMailSender
+    {
+        bool SendMail(string toAddress, string message);
+    }
+
+    public class MailSender : IMailSender
+    {
+        public bool SendMail(string toAddress, string message)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+
+            mail.From = new MailAddress("your_email@gmail.com");
+            mail.To.Add(toAddress);
+            mail.Subject = "Test Mail";
+            mail.Body = message;
+
+            smtpServer.Port = 587;
+            smtpServer.Credentials = new NetworkCredential("username", "password");
+            smtpServer.EnableSsl = true;
+
+            // Uncomment only when you want to send a real email
+            // smtpServer.Send(mail);
+
+            return true;
+        }
+    }
+
+    public class CustomerComm
+    {
+        private readonly IMailSender _mailSender;
+
+        public CustomerComm(IMailSender mailSender)
+        {
+            _mailSender = mailSender;
+        }
+
+        public bool SendMailToCustomer()
+        {
+            return _mailSender.SendMail("cust123@abc.com", "Some Message");
+        }
+    }
+}
